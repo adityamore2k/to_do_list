ce3a239ab2c06bacfd1318001b2cbd71077b6521
Aditya <62210603+adityadmore1947@users.noreply.github.com>
2022-05-12 23:07:00 +0530

    removed redux skeleton

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import Todos from "./Todos";
+
+export default function App() {
+  return (
+    <Todos />
+  );
+}
--- src/pages/ImportantTodos.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ImportantTodos() {
-  return <h1>All important todos listed here</h1>;
-}
